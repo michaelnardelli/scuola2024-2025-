17f278caec0aaf559b927b015468d11514a272d8
jacopocomper <jacopo.comper@marconirovereto.it>
2024-09-19 12:54:47 +0200

    ciao a domanii

--- autonomia/python/persona.py
+++ autonomia/python/persona.py
@@ -3,11 +3,11 @@ class persona():
         self.__nome = nome
         self.__cognome = cognome
     def __str__(self):
-        print(f"nome:{self.__nome} congome:{self.__cognome}")
+        return(f"nome:{self.__nome} congome:{self.__cognome}")
     def info(self):
         print("ciao mi chiamo {self.__nome}")
 
-info()
+
 
 
 
--- autonomia/python/test.py
+++ autonomia/python/test.py
@@ -1,1 +1,6 @@
 import persona;
+
+p1 = persona.persona("ciao","diaro")
+print(p1)
+type(p1)
+
